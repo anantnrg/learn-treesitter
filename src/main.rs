388a83b47ffe584da7a935ba56822b697b5fb892
Anant Narayan <anantnrg@gmail.com>
2024-02-17 12:13:20 +0530

    Treesitter works

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
+use tree_sitter::{Parser, Point};
+use tree_sitter_rust::language;
+
 fn main() {
+    // Initialize source
     let code = r#"fn main() {
     println!("hello world");
 }"#;
     let rope = ropey::Rope::from(code);
 
-    println!("{rope}");
+    // Initialize treesitter
+    let mut parser = Parser::new();
+    parser
+        .set_language(language())
+        .expect("Couldnt set the language");
+
+    let tree = parser
+        .parse_with(
+            &mut |byte: usize, position: Point| -> &[u8] {
+                if byte < rope.len_bytes() {
+                    rope.slice(byte..).as_str().unwrap().as_bytes()
+                } else {
+                    &[]
+                }
+            },
+            None,
+        )
+        .unwrap();
+
+    println!("{:?}", tree.root_node());
 }
